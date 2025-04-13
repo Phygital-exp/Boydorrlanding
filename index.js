77b0272f4b5c134599661a40c7dc538d898ca6a8
Phygital-exp <expphygital@gmail.com>
2025-04-13 12:15:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const fetch = require("node-fetch");
 const app = express();
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT;
 const AUTH_HEADERS = {
     Authorization: "Token 4e15396f99ae10dd5c195d81fb6a3722c0a44a10",
     "Content-Type": "application/json",
@@ -24,4 +24,9 @@ app.get("/api/boydorr/pdv", async (req, res) => {
         console.error("Error en el proxy Boydorr PDV:", err);
         res.status(500).json({ error: "Error al obtener datos de Boydorr PDV" });
     }
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en puerto ${PORT}`);
 });
